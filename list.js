ca2470d31daf74e2cb61bd0fbedfa444d641ac70
Серёжа <84621035+SergeyAksakov@users.noreply.github.com>
2022-11-19 11:59:17 +0500

    Update list.js

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let list = ["task-1", "task-2", "task-3", "task-4"];//какие задачи есть 
-class TODO {
+class task {
 filterTask(task) {
     list = list.filter((a) => {return a != task});//удаление(фильтрация) 
 }
@@ -18,5 +18,5 @@ console.log("Tasks was here + /n", list);
 const readline = require("readline-sync")();//подключение пакета
 const task = readline("task");
 
-new TODO().filterTask(task);
+new task().filterTask(task);
 console.log(list);
